298436fd11917d165d01f3f1f4c95eef800b5516
Alvi399 <muhammadalvi.23339@mhs.unesa.ac.id>
2023-11-16 17:54:15 +0700

    Fix bug in login workflow
    - Updated the login function to correctly handle edge cases, ensuring
    that users can now successfully log in without encountering any errors
    or glitches.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int a,b = 11,10;
+    int a = 20;
+    int b = 10;
     cout << "Hello World";
     cout << a + b;
     return 0;
